fd16f4cb0562fea8af86dceea88cca11daf2f039
vlad <daprostovseeto@gmail.com>
2021-04-23 13:27:44 +0300

    change error behavior

--- TPscan.py
+++ TPscan.py
@@ -17,14 +17,15 @@ def main():
     print('Start!')
     args = parse_args()
     if not acknowledge_url(url=args.url):
-        exit(1)
-    url = parse_base_url(url=args.url)
-    tasks = [asyncio.ensure_future(func(url)) for _, func in getmembers(plugins, isfunction)]
-    loop = asyncio.get_event_loop()
-    results = parse_results(results=list(loop.run_until_complete(asyncio.gather(*tasks))))
+        results = []
+    else:
+        url = parse_base_url(url=args.url)
+        tasks = [asyncio.ensure_future(func(url)) for _, func in getmembers(plugins, isfunction)]
+        loop = asyncio.get_event_loop()
+        results = parse_results(results=list(loop.run_until_complete(asyncio.gather(*tasks))))
     with open('output.json', 'w') as f:
         json.dump(results, f, indent=2)
-    print('Done!')
+    print('Finish!')
 
 
 if __name__ == '__main__':
